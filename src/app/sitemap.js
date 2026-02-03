137e1caf0c88d480969ad87a7ed5242ff470a1d3
shreyashwaghmare <shreyashwaghmare22@gmail.com>
2026-02-03 21:28:49 +0530

    sitemap update

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -10,25 +10,25 @@ export default function sitemap() {
       url: 'https://hydra-construction-services.vercel.app/services',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.9,
     },
     {
       url: 'https://hydra-construction-services.vercel.app/services/best-interior-designer-pune',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.85,
     },
     {
       url: 'https://hydra-construction-services.vercel.app/services/ahilyanagar-land-developer',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.85,
     },
     {
       url: 'https://hydra-construction-services.vercel.app/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.7,
     },
     {
       url: 'https://hydra-construction-services.vercel.app/portfolio',
@@ -40,13 +40,13 @@ export default function sitemap() {
       url: 'https://hydra-construction-services.vercel.app/portfolio-full',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.9,
+      priority: 0.8,
     },
     {
       url: 'https://hydra-construction-services.vercel.app/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.9,
+      priority: 0.8,
     },
   ]
 }
